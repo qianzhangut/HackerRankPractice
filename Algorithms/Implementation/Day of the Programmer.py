f8abf5b9a7bf334af4eecd9509016faa39717603
Qian <zhangqiandut@gmail.com>
2020-04-06 12:32:51 -0400

    day of the programmer2

--- Algorithms/Implementation/Day of the Programmer.py
+++ Algorithms/Implementation/Day of the Programmer.py
@@ -16,6 +16,8 @@ def dayOfProgrammer(year):
         leap = '12.09.{}'.format(year) 
     elif year>=1918 and year%4 == 0 and year%100 !=0:
         leap = '12.09.{}'.format(year) 
+    elif year == 1918:
+        leap = '26.09.{}'.format(year) 
     else:
         leap = '13.09.{}'.format(year)  
     return leap
